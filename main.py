e6b81633fad2f1b09e2167ffa1d6291aff22ae65
Seongjun Hong <hsj0512@snu.ac.kr>
2026-02-24 20:54:10 +0900

    feat: select block type

--- main.py
+++ main.py
@@ -1,144 +1,8 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-import threading
 import time
 import rtmidi
-
-# ─────────────────────────────────────────────────────────────
-# GP-100 SysEx Protocol Constants (reverse-engineered)
-# ─────────────────────────────────────────────────────────────
-
-HEADER = [0xF0, 0x21, 0x25, 0x7F, 0x47, 0x50, 0x2D, 0x64, 0x12] # 9 bytes
-
-# Block IDs
-BLOCKS = {
-    'PRE': 0x01,
-    'DST': 0x02,
-    'AMP': 0x03,
-    'NR':  0x04,
-    'CAB': 0x05,
-    'EQ':  0x06,
-    'MOD': 0x07,
-    'DLY': 0x08,
-    'RVB': 0x09,
-}
-
-# Effect types per block: (category, subtype)
-# Format: 'Name': (cat, sub)
-PRE_TYPES = {
-    'PRE0':  (0x00, 0x00),
-    'PRE1':  (0x00, 0x01),
-    'PRE2':  (0x01, 0x0A),
-    'PRE3':  (0x00, 0x01),
-    'PRE4':  (0x00, 0x0F),
-    'PRE5':  (0x01, 0x05),
-    'PRE6':  (0x00, 0x01),
-    'PRE7':  (0x00, 0x08),
-    'PRE8':  (0x02, 0x01),
-    'PRE9':  (0x02, 0x03),
-    'PRE10': (0x02, 0x04),
-    'PRE11': (0x03, 0x03),
-    'PRE12': (0x01, 0x09),
-    'PRE13': (0x02, 0x0F),
-}
-
-DST_TYPES = {
-    'DST0':  (0x00, 0x08),
-    'DST1':  (0x00, 0x0B),
-    'DST2':  (0x01, 0x00),
-    'DST3':  (0x02, 0x09),
-    'DST4':  (0x02, 0x0D),
-    'DST5':  (0x00, 0x00),
-    'DST6':  (0x00, 0x02),
-    'DST7':  (0x00, 0x06),
-    'DST8':  (0x00, 0x09),
-    'DST9':  (0x02, 0x02),
-    'DST10': (0x02, 0x04),
-    'DST11': (0x02, 0x0B),
-    'DST12': (0x03, 0x0F),
-    'DST13': (0x02, 0x0A),
-    'DST14': (0x03, 0x00),
-    'DST15': (0x04, 0x00),
-    'DST16': (0x01, 0x0A),
-}
-
-AMP_TYPES = {
-    'AMP0':  (0x00, 0x01),
-    'AMP1':  (0x00, 0x03),
-    'AMP2':  (0x00, 0x04),
-    'AMP3':  (0x01, 0x09),
-    'AMP4':  (0x01, 0x01),
-    'AMP5':  (0x01, 0x0A),
-    'AMP6':  (0x01, 0x04),
-    'AMP7':  (0x01, 0x05),
-    'AMP8':  (0x01, 0x0B),
-    'AMP9':  (0x01, 0x0F),
-    'AMP10': (0x02, 0x02),
-    'AMP11': (0x02, 0x0A),
-    'AMP12': (0x02, 0x0F),
-    'AMP13': (0x03, 0x05),
-    'AMP14': (0x04, 0x00),
-    'AMP15': (0x04, 0x09),
-    'AMP16': (0x03, 0x0D),
-    'AMP17': (0x02, 0x04),
-    'AMP18': (0x02, 0x07),
-    'AMP19': (0x02, 0x08),
-    'AMP20': (0x04, 0x08),
-    'AMP21': (0x04, 0x07),
-    'AMP22': (0x04, 0x0A),
-    'AMP23': (0x04, 0x0B),
-    'AMP24': (0x03, 0x09),
-    'AMP25': (0x03, 0x0A),
-    'AMP26': (0x07, 0x0C),
-    'AMP27': (0x06, 0x05),
-    'AMP28': (0x05, 0x0F),
-    'AMP29': (0x05, 0x0A),
-    'AMP30': (0x05, 0x09),
-    'AMP31': (0x05, 0x05),
-    'AMP32': (0x06, 0x08),
-    'AMP33': (0x06, 0x03),
-    'AMP34': (0x05, 0x0D),
-    'AMP35': (0x05, 0x03),
-    'AMP36': (0x06, 0x09),
-    'AMP37': (0x06, 0x0D),
-    'AMP38': (0x06, 0x0E),
-    'AMP39': (0x07, 0x03),
-    'AMP40': (0x07, 0x06),
-    'AMP41': (0x07, 0x05),
-    'AMP42': (0x07, 0x05),
-    'AMP43': (0x07, 0x07),
-    'AMP44': (0x07, 0x0A),
-}
-NR_TYPES  = {f'NR{i}':  (i // 16, i % 16) for i in range(4)}
-CAB_TYPES = {f'CAB{i}': (i // 16, i % 16) for i in range(8)}
-EQ_TYPES  = {f'EQ{i}':  (i // 16, i % 16) for i in range(4)}
-MOD_TYPES = {f'MOD{i}': (i // 16, i % 16) for i in range(8)}
-DLY_TYPES = {f'DLY{i}': (i // 16, i % 16) for i in range(8)}
-RVB_TYPES = {f'RVB{i}': (i // 16, i % 16) for i in range(8)}
-
-BLOCK_TYPES = {
-    'PRE': PRE_TYPES,
-    'DST': DST_TYPES,
-    'AMP': AMP_TYPES,
-    'NR':  NR_TYPES,
-    'CAB': CAB_TYPES,
-    'EQ':  EQ_TYPES,
-    'MOD': MOD_TYPES,
-    'DLY': DLY_TYPES,
-    'RVB': RVB_TYPES,
-}
-
-BLOCK_COLORS = {
-    'PRE': '#E74C3C',
-    'DST': '#E67E22',
-    'AMP': '#F1C40F',
-    'NR':  '#2ECC71',
-    'CAB': '#1ABC9C',
-    'EQ':  '#3498DB',
-    'MOD': '#9B59B6',
-    'DLY': '#E91E63',
-    'RVB': '#00BCD4',
-}
+from constants import HEADER, BLOCKS, BLOCK_TYPES, BLOCK_COLORS
 
 # ─────────────────────────────────────────────────────────────
 # MIDI Communication
@@ -209,22 +73,28 @@ class GP100MIDI:
         """Set effect type for a block"""
         block_id = BLOCKS[block_name]
         types = BLOCK_TYPES[block_name]
-        if type_name not in types:
-            return
-        cat, sub = types[type_name]
-        data = [
-            0x10, block_id, 0x00, 0x01,
-            cat, sub,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            state_byte
+        type_tup = types[type_name]
+        settype_header = [
+            [0x00, 0x00, 0x10, 0x00, 0x0F, 0x0F, 0x0F, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x10, 0x03, 0x0F, 0x0F, 0x0F, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+            [0x00, 0x00, 0x10, 0x06, 0x0F, 0x0F, 0x0F, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         ]
-        self.send_sysex(data)
+        data = [
+            [0x10, block_id, 0x00, 0x01, type_tup[0], type_tup[1], 0x00, 0x00, 0x00, 0x00, 0x00, type_tup[2]],
+            [0x10, block_id, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00]
+        ]
+        self.send_sysex(settype_header[0])
+        self.send_sysex(settype_header[1])
+        self.send_sysex(settype_header[2])
+        self.send_sysex(data[0])
+        self.send_sysex(data[1])
 
     def set_patch(self, patch_number):
         """Switch to a patch (1-based)"""
         data = [
-            0x00, 0x02, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, patch_number, 0x00, 0x00
+            0x00, 0x02, 
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+            patch_number, 0x00, 0x00
         ]
         self.send_sysex(data)
 
@@ -352,7 +222,7 @@ class GP100Editor(tk.Tk):
         for i in range(1, 9):
             tk.Radiobutton(patch_frame, text=str(i),
                           variable=self.patch_var, value=i,
-                          command=lambda p=i: self._send_patch(p),
+                          command=lambda p=i: self._send_patch(p-1),
                           bg='#1A1A2E', fg='#C0C0E0',
                           selectcolor='#0F3460',
                           font=('Courier', 11),
